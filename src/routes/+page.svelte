65594b4a8bc022ad0324aa6b2573b16a5e730471
George Enciu <george.enciu@gmail.com>
2023-09-02 10:48:51 +0200

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let data;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Today is {data.today}</p>
+
+<a href="/about">About</a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let data;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Today is {data.today}</p>
